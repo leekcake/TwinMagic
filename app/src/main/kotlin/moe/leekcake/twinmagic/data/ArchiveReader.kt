4d3aa6b92033f8714dc8b556c9b33374fd242430
Blue Electric <fkdlxmslrx@gmail.com>
2019-03-05 20:33:24 +0900

    Implement mergeTweetView

---
+++ app/src/main/kotlin/moe/leekcake/twinmagic/data/ArchiveReader.kt
@@ -0,0 +1,130 @@
+package moe.leekcake.twinmagic.data
+
+import org.json.simple.JSONArray
+import org.json.simple.JSONObject
+import org.json.simple.parser.JSONParser
+import java.io.File
+import java.io.FileOutputStream
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
+
+class ArchiveReader() {
+    val statuses = HashMap<Long, JSONObject>()
+
+    var statuses_dates = ArrayList<String>()
+    var statues_datefilter = HashMap<String, ArrayList<Long>>()
+
+    companion object {
+        private fun cutJsonVariableSetter(sJson: String): String {
+            return sJson.substring(sJson.indexOf('=') + 1)
+        }
+
+        private val archiveFormat = SimpleDateFormat("yyyy-MM-dd hh:mm:ss Z")
+        private val twitter4jFormat = SimpleDateFormat("EEE MMM d HH:mm:ss z yyyy", Locale.US)
+
+        private fun FixStatus(obj: JSONObject) {
+            val stringDate = obj["created_at"] as String
+            var created_at: Date? = null
+
+            try {
+                created_at = archiveFormat.parse(stringDate)
+            } catch (e: ParseException) {
+                try {
+                    created_at = twitter4jFormat.parse(stringDate)
+                } catch (e2: ParseException) {
+                    throw e2
+                }
+            }
+
+            obj["created_at"] = twitter4jFormat.format(created_at)
+            if (obj.containsKey("entities")) {
+                (obj["entities"] as JSONObject).remove("media")
+            }
+            if (obj.containsKey("retweeted_status")) {
+                FixStatus(obj["retweeted_status"] as JSONObject)
+            }
+        }
+    }
+
+    fun export(outputDirectory: File) {
+        //TODO: Copy archive reader (from twitter archive)
+
+        val payload = JSONObject()
+        payload["tweets"] = statuses.size
+        val sdf = SimpleDateFormat("YYYY-MM-DD hh:mm:ss")
+        payload["created_at"] = sdf.format(Date())
+        payload["lang"] = "ko"
+
+        val js = File(outputDirectory,"data/js")
+        js.mkdirs()
+
+        writeJson(File(outputDirectory, "data/js/payload_details.js"), payload.toJSONString(), "var payload_details = ")
+
+        val tweet_index = JSONArray()
+        val tweets = File(outputDirectory, "data/js/tweets")
+        tweets.deleteRecursively()
+        tweets.mkdirs()
+        for (date in statuses_dates) {
+            val data = JSONObject()
+            data["year"] = java.lang.Long.parseLong(date.substring(0, 4))
+            data["month"] = java.lang.Long.parseLong(date.substring(5, 7))
+            data["file_name"] = "data/js/tweets/$date.js"
+            data["var_name"] = "tweets_$date"
+            data["tweet_count"] = statues_datefilter.get(date)?.size
+            tweet_index.add(data)
+
+            val tweets_json = File(tweets, "$date.js")
+            val tweets_ja = JSONArray()
+            for (ID in statues_datefilter.get(date)!!) {
+                tweets_ja.add(statuses[ID])
+            }
+            writeJson(tweets_json, tweets_ja.toJSONString(), "Grailbird.data.tweets_$date = ")
+        }
+        writeJson(File(outputDirectory, "data/js/tweet_index.js"), tweet_index.toJSONString(), "var tweet_index = ")
+    }
+
+    private fun writeJson(json: File, jsonString: String, setter: String) {
+        val fos = FileOutputStream(json)
+
+        fos.write(setter.toByteArray())
+        fos.write(jsonString.toByteArray())
+
+        fos.close()
+    }
+
+    fun readFolder(directory: File) {
+        for (file in File(directory, "data/js/tweets").listFiles()!!) {
+            readJson(file)
+        }
+
+        statuses_dates.sortWith(Collections.reverseOrder())
+        for (ids in statues_datefilter.values) {
+            ids.sortWith(Collections.reverseOrder())
+        }
+    }
+
+    private fun readJson(json: File) {
+        val date = json.name.replace(".js", "")
+        val sJson = cutJsonVariableSetter(json.readText())
+
+        val parser = JSONParser()
+        val statuses = parser.parse(sJson) as JSONArray
+
+        for (obj in statuses) {
+            val status = obj as JSONObject
+            FixStatus(status)
+            val id = status["id"] as Long
+
+            if (!statues_datefilter.containsKey(date)) {
+                statuses_dates.add(date)
+                statues_datefilter[date] = ArrayList()
+            }
+            statues_datefilter[date]!!.add(id)
+
+            if (!this.statuses.containsKey(id)) {
+                this.statuses[id] = status
+            }
+        }
+    }
+}
--- app/src/main/kotlin/moe/leekcake/twinmagic/view/MergeTweetViewController.kt
+++ app/src/main/kotlin/moe/leekcake/twinmagic/view/MergeTweetViewController.kt
@@ -6,7 +6,11 @@ import javafx.scene.control.Button
 import javafx.scene.control.Label
 import javafx.scene.control.ListView
 import javafx.scene.control.ProgressBar
-import javafx.stage.FileChooser
+import javafx.stage.DirectoryChooser
+import moe.leekcake.twinmagic.data.ArchiveReader
+import java.io.File
+import java.io.FileOutputStream
+
 
 class MergeTweetViewController {
     @FXML
@@ -32,10 +36,35 @@ class MergeTweetViewController {
     }
 
     fun handleAddArchiveButton() {
-        val fileChooser = FileChooser()
-        fileChooser.title = "아카시브 파일 선택"
-        val file = fileChooser.showOpenDialog( addArchiveButton.scene.window ) ?: return
+        val directoryChooser = DirectoryChooser()
+        directoryChooser.title = "아카시브 폴더 선택"
+        val directory = directoryChooser.showDialog( addArchiveButton.scene.window ) ?: return
 
-        archiveListViewItems.add(file.path)
+        archiveListViewItems.add(directory.path)
+    }
+
+    fun handleMergeArchiveButton() {
+        val directoryChooser = DirectoryChooser()
+        directoryChooser.title = "합친 아카시브가 저장될 폴더 선택"
+        val directory = directoryChooser.showDialog( addArchiveButton.scene.window ) ?: return
+
+        val flag = File(directory, "combined.bin")
+
+        if(directory.list().isNotEmpty()) {
+            if( !flag.exists() ) {
+                //TODO: Warning for Not empty folder without flag
+                return
+            }
+        }
+
+        val archiveReader = ArchiveReader()
+
+        for(path in archiveListViewItems) {
+            archiveReader.readFolder( File(path) )
+        }
+
+        archiveReader.export(directory)
+        //Generate flag(empty) file
+        FileOutputStream(flag).close()
     }
 }
