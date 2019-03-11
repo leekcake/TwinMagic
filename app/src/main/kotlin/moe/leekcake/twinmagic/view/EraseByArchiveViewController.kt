9cce51aa0031cf946cfbf7e4dc973afc68729f90
Blue Electric <fkdlxmslrx@gmail.com>
2019-03-11 22:19:18 +0900

    Refactor statusLabel into progressLabel

--- app/src/main/kotlin/moe/leekcake/twinmagic/view/EraseByArchiveViewController.kt
+++ app/src/main/kotlin/moe/leekcake/twinmagic/view/EraseByArchiveViewController.kt
@@ -1,9 +1,10 @@
 package moe.leekcake.twinmagic.view
 
+import javafx.application.Platform
 import javafx.collections.FXCollections
 import javafx.fxml.FXML
+import javafx.scene.control.Label
 import javafx.scene.control.ListView
-import javafx.stage.DirectoryChooser
 import moe.leekcake.tweettail.desktop.view.data.status.StatusCell
 import moe.leekcake.twinmagic.data.ArchiveReader
 import org.json.simple.JSONObject
@@ -15,20 +16,36 @@ class EraseByArchiveViewController {
     private val listViewItems = FXCollections.observableArrayList<JSONObject>()
 
     @FXML
+    lateinit var progressLabel : Label
+
+    @FXML
     private fun initialize() {
         listView.cellFactory = StatusCell.newStatusCellFactory()
         listView.items = listViewItems
     }
 
     fun setArchiveDirectory(directory: File) {
-        val archiveReader = ArchiveReader()
-        archiveReader.readFolder(directory)
+        val reader = Thread {
+            val archiveReader = ArchiveReader()
+            archiveReader.readFolder(directory, object: ArchiveReader.ProgressReceiver {
+                override fun onArchiveReaderProgress(progress: Double, status: String) {
+                    Platform.runLater {
+                        progressLabel.text = "$status (${progress * 100}%)"
+                    }
+                }
+            })
 
-        val items = ArrayList<JSONObject>(archiveReader.statuses.size)
-        items.addAll( archiveReader.statuses.values )
+            val items = ArrayList<JSONObject>(archiveReader.statuses.size)
+            items.addAll( archiveReader.statuses.values )
 
-        items.sortWith(compareBy { it["id"] as Long })
+            items.sortWith(compareBy { it["id"] as Long })
 
-        listViewItems.addAll(items)
+            Platform.runLater {
+                listViewItems.addAll(items)
+                progressLabel.isVisible = false
+            }
+        }
+
+        reader.start()
     }
 }
